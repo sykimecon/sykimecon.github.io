99a54ddda5a2db8723887f376b1765330b27abc9
president <abanda435@gmail.com>
2024-03-01 00:15:17 +0200

    add nojekyll file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "",
+    assetPrefix: process.env.ASSET_PREFIX,
+    basePath: process.env.BASE_PATH,
 };
 
 export default nextConfig;
